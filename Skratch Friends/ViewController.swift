c96061ad8cefcb3b352a05a57011ba3487ef4a8f
Tymoteusz Pasieka <tymoteusz.pasieka@gmail.com>
2021-04-23 13:08:22 +0200

    Initial Commit

---
+++ Skratch Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skratch Friends
+//
+//  Created by Tymoteusz Pasieka on 23/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
