51aa48be28759cf435927e21f7bce2ba3ad6acc1
Tymoteusz Pasieka <tymoteusz.pasieka@gmail.com>
2021-04-27 13:55:03 +0200

    #text field polishment #added app icon

--- Skratch Friends/Views/PaddingTextField.swift
+++ Skratch Friends/Views/PaddingTextField.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class PaddingTextField: UITextField {
     var applyPadding: Bool = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
     private let padding = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
 
@@ -23,4 +32,23 @@ class PaddingTextField: UITextField {
     override open func editingRect(forBounds bounds: CGRect) -> CGRect {
         return applyPadding ? bounds.inset(by: padding) : bounds
     }
+    
+    private func configure() {
+        self.layer.cornerRadius = 24
+        self.layer.masksToBounds = true
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.textColor = UIColor(red: 0.165, green: 0.18, blue: 0.263, alpha: 1)
+        
+        self.font = UIFont(name: "CircularStd-Medium", size: 24)
+        
+        self.textAlignment = .center
+        self.keyboardType = .numberPad
+        self.placeholder = "No. of users"
+    }
+    
+    func changeFontSize(shrink: Bool) {
+        self.font = shrink ? UIFont(name: "CircularStd-Medium", size: 17) : UIFont(name: "CircularStd-Medium", size: 24) 
+    }
+    
 }
